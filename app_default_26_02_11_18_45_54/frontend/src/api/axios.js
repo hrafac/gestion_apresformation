39bcf9c15177fa8caed5e5b9aa596c9b780b60cd
flash <hharrare@gmail.com>
2026-04-06 02:45:25 +0100

    Fix CORS - correct backend URL in frontend axios configuration

--- app_default_26_02_11_18_45_54/frontend/src/api/axios.js
+++ app_default_26_02_11_18_45_54/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://gestion-backend.onrender.com'
+    baseURL: 'https://gestion-apresformation-backend.onrender.com/api'
 });
 
 instance.interceptors.request.use((config) => {
